59c110d5c685d232569ddde6709797e32931bfbc
Morgan Grubb <morgan@wishpond.com>
2012-05-05 09:11:16 -0700

    Inital commit of a raw application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bluesfling::Application.config.secret_token = 'a9e17b77aa74d5ac1f0458539dfa99d4016f103631442a3a2b00b67c79e8af071a77c9cd83a7e0836808a38b9e011ae2055e2bfe086444749e0d4a2bd0213d0a'
