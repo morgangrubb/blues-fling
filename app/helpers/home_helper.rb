5b455b5800b5922079867c1c72b752785d605b2f
Morgan Grubb <morgan@wishpond.com>
2012-05-11 18:36:30 -0700

    Return false on window open

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,7 +1,7 @@
 module HomeHelper
 
   def registration_link
-    link_to 'Register now!', '#', class: 'btn btn-primary btn-large', onclick: %(open("http://bluesfling3.dancecamps.org/booking.php","","toolbar=yes, location=no, directories=no, menubar=no, scrollbars=yes, resizable=yes, copyhistory=no, width=840, height=550"))
+    link_to 'Register now!', '#', class: 'btn btn-primary btn-large', onclick: %(open("http://bluesfling3.dancecamps.org/booking.php","","toolbar=yes, location=no, directories=no, menubar=no, scrollbars=yes, resizable=yes, copyhistory=no, width=840, height=550"); return false;)
   end
 
 end
