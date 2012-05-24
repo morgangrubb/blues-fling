067f7096198b26818f4aeb3bca6f9320e1e4750e
Morgan Grubb <morgan@wishpond.com>
2012-05-24 00:32:49 -0700

    Private lessons and David's Loft

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+  def map_image(location, options = {})
+    options.reverse_merge!({
+      markers: location,
+      center: location,
+      zoom: 15,
+      size: '300x200',
+      sensor: 'false'
+    })
+
+    image = tag(:img, src: "http://maps.googleapis.com/maps/api/staticmap?#{options.to_param}")
+    link_to image, map_link(location), target: '_blank'
+  end
+
+  def map_link(location)
+    "http://maps.google.com/maps?q=#{location}&hl=en"
+  end
+
 end
