bc2449cb7360dc8ce56dc1b939d0d5d0002509d6
Morgan Grubb <morgan@wishpond.com>
2012-06-15 07:16:29 -0700

    Closed registration links

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,7 +1,8 @@
 module HomeHelper
 
   def registration_link
-    link_to 'Register now!', '#', class: 'btn btn-primary btn-large', onclick: %(open("http://bluesfling3.dancecamps.org/booking.php","","toolbar=yes, location=no, directories=no, menubar=no, scrollbars=yes, resizable=yes, copyhistory=no, width=840, height=550"); return false;)
+    # link_to 'Register now!', '#', class: 'btn btn-primary btn-large', onclick: %(open("http://bluesfling3.dancecamps.org/booking.php","","toolbar=yes, location=no, directories=no, menubar=no, scrollbars=yes, resizable=yes, copyhistory=no, width=840, height=550"); return false;)
+    link_to 'Registration is now closed', '#', class: 'btn btn-primary btn-large', onclick: "return false;", disabled: true
   end
 
 end
