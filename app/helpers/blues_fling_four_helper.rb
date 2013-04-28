79039dc1b52e5c73685da50cae60e8cdc36be530
Morgan Grubb <miggles@gmail.com>
2013-04-28 15:57:56 -0700

    Updated to completely new style for main site.

--- app/helpers/blues_fling_four_helper.rb
+++ app/helpers/blues_fling_four_helper.rb
@@ -1,8 +1,33 @@
 module BluesFlingFourHelper
 
-  def registration_link
-    link_to 'Register now!', '#', class: 'button button-big', onclick: %(open("http://bluesflingfusion.dancecamps.org/booking.php","","toolbar=yes, location=no, directories=no, menubar=no, scrollbars=yes, resizable=yes, copyhistory=no, width=840, height=550"); return false;)
+  def registration_link(options = {})
+    link_to 'Register now!', '#', class: "btn btn-warning btn-large", onclick: %(open("http://bluesflingfusion.dancecamps.org/booking.php","","toolbar=yes, location=no, directories=no, menubar=no, scrollbars=yes, resizable=yes, copyhistory=no, width=840, height=550"); return false;)
     # link_to 'Registration is now closed', '#', class: 'btn btn-primary btn-large', onclick: "return false;", disabled: true
+  end
+
+  def navbar_links
+    base_url_options = { controller: "blues_fling_four" }
+
+    links = [
+      # ["Home",                      { action: "index" }.reverse_merge(base_url_options)],
+      ["Instructors",               { action: "instructors" }.reverse_merge(base_url_options)],
+      ["Schedule",                  { action: "schedule" }.reverse_merge(base_url_options)],
+      ["Pricing and Registration",  { action: "registration" }.reverse_merge(base_url_options)],
+      ["FAQ",                       { action: "faq" }.reverse_merge(base_url_options)]
+    ]
+
+    parsed_links = links.collect do |link_options|
+      active =
+        if link_options[1].is_a? Hash
+          params[:action] == link_options[1][:action]
+        else
+          false
+        end
+
+      content_tag :li, link_to(*link_options), class: "#{'active' if active}"
+    end
+
+    parsed_links.join(content_tag(:li, "", class: "divider-vertical")).html_safe
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Bluesfling::Application.routes.draw do
 
-  match 'blues-fling-4(/:action)', controller: "blues_fling_four", action: /(index|sample|left-sidebar|no-sidebar|right-sidebar)/
+  match 'blues-fling-4(/:action)', controller: "blues_fling_four", action: /(instructors|schedule|private_lessons|registration|contact_us|faq)/
 
   match 'blues-fling-3(/:action)', controller: "blues_fling_three", action: /(instructors|schedule|private_lessons|registration|contact_us)/
 
-  # get 'instructors' => 'home#instructors'
-  # get 'schedule' => 'home#schedule'
-  # get 'private_lessons' => 'home#private_lessons'
-  # get 'registration' => 'home#registration'
-  # get 'contact_us' => 'home#contact_us'
+  # Change this to set the current event.
+  current_event = "blues_fling_four"
 
-  root :to => 'blues_fling_four#index'
+  get 'instructors'     => "#{current_event}#instructors"
+  get 'schedule'        => "#{current_event}#schedule"
+  get 'private_lessons' => "#{current_event}#private_lessons"
+  get 'registration'    => "#{current_event}#registration"
+  get 'faq'             => "#{current_event}#faq"
+  get 'contact_us'      => "#{current_event}#contact_us"
+
+  root :to => "#{current_event}#index"
 end
