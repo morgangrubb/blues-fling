145a9223e602237548133d961a1ec64dd892b0ec
Morgan Grubb <miggles@gmail.com>
2013-06-16 11:46:23 -0700

    Added private lessons.

--- app/helpers/blues_fling_four_helper.rb
+++ app/helpers/blues_fling_four_helper.rb
@@ -10,10 +10,10 @@ module BluesFlingFourHelper
 
     links = [
       # ["Home",                      { action: "index" }.reverse_merge(base_url_options)],
-      ["Blues & Fusion",        { action: "about" }.reverse_merge(base_url_options)],
+      ["Blues & Fusion",            { action: "about" }.reverse_merge(base_url_options)],
       ["Instructors",               { action: "instructors" }.reverse_merge(base_url_options)],
       ["Schedule",                  { action: "schedule" }.reverse_merge(base_url_options)],
-      ["Pricing and Registration",  { action: "registration" }.reverse_merge(base_url_options)],
+      ["Pricing & Registration",    { action: "registration" }.reverse_merge(base_url_options)],
       ["Volunteer",                 { action: "volunteer" }.reverse_merge(base_url_options)],
       ["FAQ",                       { action: "faq" }.reverse_merge(base_url_options)]
     ]
